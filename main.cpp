a0bba272157c9d8f88b3941ac1482f8c9389d376
Vladislav <promistrio@ya.ru>
2018-06-10 22:50:10 +0300

    comments

--- main.cpp
+++ main.cpp
@@ -6,9 +6,14 @@
 #include <errno.h>   /* ERROR Number Definitions           */
 #include<common/mavlink.h>
 #include <ardupilotmega/mavlink.h>
+/*
+ *
+ * Важно не забыть выставить верный порт и скорость для USB - 115200 а для telem1,2 - 57600
+ *
+ * */
 
 int
-set_interface_attribs (int fd, int speed, int parity)
+set_interface_attribs (int fd, int speed, int parity) // В это вдаваться не нужно. Я сам не понимаю почему именно этот код в линуксе работает отменно
 {
     struct termios tty;
     memset (&tty, 0, sizeof tty);
@@ -49,7 +54,7 @@ set_interface_attribs (int fd, int speed, int parity)
 }
 
 void
-set_blocking (int fd, int should_block)
+set_blocking (int fd, int should_block) // В этом тоже не нужно разбираться
 {
     struct termios tty;
     memset (&tty, 0, sizeof tty);
@@ -66,9 +71,32 @@ set_blocking (int fd, int should_block)
         printf ("error %d setting term attributes", errno);
 }
 
+void
+send_pack (int fd, float p_lat, float p_lon) // Отправка пакета
+{
+    // Общий тип для каждого mavlink сообщения. Используется для хранения неструктурированного массива данных
+    mavlink_message_t msg;
+    // это кастомное сообщение, которое я создал. Лежит в папке mavlink/ardupilotmega
+    mavlink_opt_raw_t packet;
+
+    // просто заполняем float нужными величинами
+    packet.lat = p_lat;
+    packet.lon = p_lon;
+
+    //mavlink_msg_opt_raw_encode(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg, const mavlink_opt_raw_t* opt_raw) так выглядит объявление функции
+    // заполняем 255 это наземная станция 1 - id устройства на которое отправляются данные
+    mavlink_msg_opt_raw_encode(255, 1, &msg, &packet);
+
+    // просто отправка буффера
+    uint8_t buf[MAVLINK_MAX_PACKET_LEN];
+    uint8_t len = mavlink_msg_to_send_buffer(buf, &msg);
+
+    write(fd, ( char * ) ( buf ), len);
+}
+
 int main()
 {
-
+    // <Не интересно>
     char portname[20] = "/dev/ttyACM0";
     int fd = open (portname, O_RDWR | O_NOCTTY | O_SYNC);
 
@@ -81,15 +109,11 @@ int main()
     set_interface_attribs (fd, B115200, 0);  // set speed to 115,200 bps, 8n1 (no parity)
     set_blocking (fd, 0);                // set no blocking
 
-    //write (fd, "hello!\n", 7);           // send 7 character greeting
+    // </ Не интересно>
 
-    //usleep ((7 + 25) * 100);             // sleep enough to transmit the 7 plus
-    // receive 25:  approx 100 uS per char transmit
-    /*char buf [100];
-    int n = read (fd, buf, sizeof buf);
 
-    printf ("number of chars: %d", n);*/
-
+    int w = 0; // счетчик итераций цикла ниже
+    int max_skip = 200; // Кол-во итераций циклов, которое должно быть пропущено, чтобы не спамить автопилот(см ниже)
 
 
     while (1)
@@ -97,168 +121,99 @@ int main()
         mavlink_message_t message;
         mavlink_status_t status;
 
-        char buf [200];  //Buffer to store the data received
+        char buf [200];
+        int n = read(fd, buf, sizeof buf); // просто читаем не более 200 байт, которые нам прислали
 
-        int n = read(fd, buf, sizeof buf);
-        //printf ("number of chars: %d", n);
-
-        for (int pos = 0; pos < n; pos++)
+        for (int pos = 0; pos < n; pos++) // обрабатываем каждый байт. Этот цикл для функции mavlink_parse_char. Она принимает по одному байту.
         {
             if (mavlink_parse_char(MAVLINK_COMM_0, (uint8_t)buf[pos],
                                    &message, &status))
             {
-                if (message.msgid == MAVLINK_MSG_ID_HEARTBEAT)
-                {
-                    printf("\n\n  ");
+                // Если после вновь добавленного байта получилось целостное сообщение, то возвращает true и можно парсить
 
+                //Обрабатываем только нужные нам сообщения
+                if (message.msgid == MAVLINK_MSG_ID_HEARTBEAT) // сердцебиение
+                {
                     mavlink_heartbeat_t heartbeat;
                     mavlink_msg_heartbeat_decode(&message, &heartbeat);
 
+                    printf("\n\n  ");
                     printf("Heartbeat received, system type:%d System status: %d", heartbeat.type, heartbeat.system_status);
                     printf("\n +----------------------------------+\n");
                 }
 
+                // это то самое новое сообщение. Здесь происходит разбор принятого пакета
                 if (message.msgid == MAVLINK_MSG_ID_OPT_RAW)
                 {
-                    printf("\n\n  ");
-
                     mavlink_opt_raw_t m_opt_raw;
                     mavlink_msg_opt_raw_decode(&message, &(m_opt_raw));
 
+                    // По сути это и есть ЭХО. Выведет ранее отправленные широту и долготу (функция send_pack ниже это как раз и делает)
+                    // Если send_pack ниже закомментировать будут те значения, которые я инициализировал в автопилоте. Т.е. нули.
+                    printf("\n\n  ");
                     printf("lat:%f lon: %f", m_opt_raw.lat , m_opt_raw.lon);
                     printf("\n +----------------------------------+\n");
+
+                    //Если этого не достаточно, можно подключиться к автопилоту по USB и через telem1. И проверять выход их другого порта
+                    //В данном случае проще позвонить мне, я расскажу.
+
                     break;
                 }
+
+                //Ну и для интереса стандартные пакеты с положением аппарата
+                //GPS
+                if (message.msgid == MAVLINK_MSG_ID_GPS_RAW_INT)
+                {
+                    mavlink_gps_raw_int_t gps_raw;
+                    mavlink_msg_gps_raw_int_decode(&message, &(gps_raw));
+                    //qDebug() <<"lat: " << gps_raw.lat / 1.0e7 << " lon: " << gps_raw.lon / 1.0e7 << " alt: " << gps_raw.alt;
+
+                    float m_lat = gps_raw.lat / 1.0e7f; // Э-Экономия. В этом пакете широта и долгота хранится в shortint
+                    float m_lon = gps_raw.lon / 1.0e7f;
+
+                    printf("\n\n  ");
+                    printf("GPS lat:%f lon: %f", m_lat , m_lon);
+                    printf("\n +----------------------------------+\n");
+                }
+
+                //Крен тангаж рысканье
+                //Ниже на qt.
+                /*
+                if (message.msgid == MAVLINK_MSG_ID_ATTITUDE && message.sysid != 0)
+                            {
+                                mavlink_attitude_t attitude;
+                                mavlink_msg_attitude_decode(&message, &attitude);
+
+                                QVariant pitch(qRadiansToDegrees(attitude.pitch));
+                                QVariant roll(qRadiansToDegrees(attitude.roll));
+                                QVariant yaw(qRadiansToDegrees(attitude.yaw));
+                                m_pitch = pitch;
+                                m_roll = roll;
+                                m_yaw = yaw;
+                }
+                */
+
+
             }
         }
+
+        if(w == max_skip){
+            // чтобы слишком часто не отправлять команду пришлось сделать такой костыль. Нужно как-то это дело в отдельный поток.
+            // могу помочь сделать это на qt
+
+            //Если закомментировать, то ответное сообщение будет содержать нули
+
+            send_pack(fd, 5.0f, 20.0f); // отправляете рандомные float значения для ЭХО.
+
+            //printf("sent");
+        }
+        else
+            w++;
+
     }
 
 
-    close(fd); /* Close the serial port */
+    close(fd);
     return 0;
 
 }
-
-//#include <stdio.h>
-//#include <fcntl.h>   /* File Control Definitions           */
-//#include <termios.h> /* POSIX Terminal Control Definitions */
-//#include <unistd.h>  /* UNIX Standard Definitions 	   */
-//#include <errno.h>   /* ERROR Number Definitions           */
-//#include<common/mavlink.h>
-//#include <ardupilotmega/mavlink.h>
-
-//int main()
-//{
-//    int fd;/*File Descriptor*/
-
-//    printf("\n +----------------------------------+");
-//    printf("\n |        Serial Port Read          |");
-//    printf("\n +----------------------------------+");
-
-//    /*------------------------------- Opening the Serial Port -------------------------------*/
-
-//    /* Change /dev/ttyUSB0 to the one corresponding to your system */
-
-//    fd = open("/dev/ttyACM0",O_RDWR | O_NOCTTY);	/* ttyUSB0 is the FT232 based USB2SERIAL Converter   */
-//    /* O_RDWR   - Read/Write access to serial port       */
-//    /* O_NOCTTY - No terminal will control the process   */
-//    /* Open in blocking mode,read will wait              */
-
-
-
-//    if(fd == -1)						/* Error Checking */
-//    {
-//        printf("\n  Error! in Opening serial");
-//        close(fd);
-//        return -1;
-//    }
-//    else
-//        printf("\n  serial Opened Successfully ");
-
-
-//    /*---------- Setting the Attributes of the serial port using termios structure --------- */
-
-//    struct termios SerialPortSettings;	/* Create the structure                          */
-
-//    tcgetattr(fd, &SerialPortSettings);	/* Get the current attributes of the Serial port */
-
-//    /* Setting the Baud rate */
-//    cfsetispeed(&SerialPortSettings,B115200); /* Set Read  Speed as 9600                       */
-//    cfsetospeed(&SerialPortSettings,B115200); /* Set Write Speed as 9600                       */
-
-//    /* 8N1 Mode */
-//    SerialPortSettings.c_cflag &= ~PARENB;   /* Disables the Parity Enable bit(PARENB),So No Parity   */
-//    SerialPortSettings.c_cflag &= ~CSTOPB;   /* CSTOPB = 2 Stop bits,here it is cleared so 1 Stop bit */
-//    SerialPortSettings.c_cflag &= ~CSIZE;	 /* Clears the mask for setting the data size             */
-//    SerialPortSettings.c_cflag |=  CS8;      /* Set the data bits = 8                                 */
-
-//    SerialPortSettings.c_cflag &= ~CRTSCTS;       /* No Hardware flow Control                         */
-//    SerialPortSettings.c_cflag |= CREAD | CLOCAL; /* Enable receiver,Ignore Modem Control lines       */
-
-
-//    SerialPortSettings.c_iflag &= ~(IXON | IXOFF | IXANY);          /* Disable XON/XOFF flow control both i/p and o/p */
-//    SerialPortSettings.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);  /* Non Cannonical mode                            */
-
-//    SerialPortSettings.c_oflag &= ~OPOST;/*No Output Processing*/
-
-//    /* Setting Time outs */
-//    SerialPortSettings.c_cc[VMIN] = 10; /* Read at least 10 characters */
-//    SerialPortSettings.c_cc[VTIME] = 0; /* Wait indefinetly   */
-
-
-//    if((tcsetattr(fd,TCSANOW,&SerialPortSettings)) != 0){ /* Set the attributes to the termios structure*/
-//        printf("\n  ERROR ! in Setting attributes");
-//        close(fd);
-//        return -2;
-//    }
-//    else
-//        printf("\n  BaudRate = 9600 \n  StopBits = 1 \n  Parity   = none");
-
-
-//    /*------------------------------- Read data from serial port -----------------------------*/
-
-//    //tcflush(fd, TCIFLUSH);   /* Discards old data in the rx buffer            */
-
-
-
-//    mavlink_message_t message;
-//    mavlink_status_t status;
-
-
-
-
-//    char read_buffer[32];   //Buffer to store the data received
-//    int  bytes_read = 0;    // Number of bytes read by the read() system call
-
-//    bytes_read = read(fd,&read_buffer,32);
-
-//    printf("\n\n  Bytes Rxed -%d", bytes_read);
-
-//    for (int pos = 0; pos < bytes_read; ++pos)
-//    {
-
-
-//        if (mavlink_parse_char(MAVLINK_COMM_0, (uint8_t)read_buffer[pos],
-//                               &message, &status))
-//        {
-//            if (message.msgid == MAVLINK_MSG_ID_HEARTBEAT)
-//            {
-//                printf("\n\n  ");
-//                mavlink_heartbeat_t heartbeat;
-//                mavlink_msg_heartbeat_decode(&message, &heartbeat);
-
-//                printf("Heartbeat received, system type:%d System status: %d", heartbeat.type, heartbeat.system_status);
-//                printf("\n +----------------------------------+\n\n\n");
-//            }
-//        }
-//    }
-//    //printf("\n\n  Bytes Rxed -%d", bytes_read);
-
-//    //for(i=0;i<bytes_read;i++)
-//    //    printf("%c",read_buffer[i]);
-
-
-//    close(fd); /* Close the serial port */
-//    return 0;
-
-//}
